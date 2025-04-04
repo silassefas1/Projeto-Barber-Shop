eec71d110248ca4db634e1a38d40b60d38055321
Silas Sefas <silassefas23@gmail.com>
2025-04-04 17:33:39 -0300

    Criando formulario de cadastro de cliente - Parte 1

--- src/app/clients/components/client-form/client-form.component.ts
+++ src/app/clients/components/client-form/client-form.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ClientModelForm } from '../../client.models';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-client-form',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgxMaskDirective
+
+
+  ],
   templateUrl: './client-form.component.html',
-  styleUrl: './client-form.component.scss'
+  styleUrls:  ['./client-form.component.scss'] 
 })
 export class ClientFormComponent {
 
+  @Input() client: ClientModelForm = {id: 0, name: '',email: '', phone: ''}
+
+  @Output() clientSubmitted = new EventEmitter<ClientModelForm>();
+
+  onSubmit(_: NgForm){
+    this.clientSubmitted.emit(this.client)
+  }
 }
--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
-import { SERVICES_TOKEN } from '../../services/service.token'
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientModelForm } from '../client.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
   providers: [
-    {provide SERVICES_TOKEN.HTTP.CLIENT, userClass: ClientsService}
-  ]
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService }
+  ],
+  
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy{
 
-  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService){
+  private httpSubscription?: Subscription
 
+  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT) private readonly httpService: IClientService,
+  private readonly router: Router
+  ){}
+
+  ngOnDestroy(): void {
+    if(this.httpSubscription){
+      this.httpSubscription.unsubscribe()
+    }
   }
+
+  onSubmitClient(value: ClientModelForm){
+    const {id, ...request} = value
+    this.httpService.save(request).subscribe(_ => {
+      this.router.navigate('clients/list')
+    })
+  }
+
+  
 }
